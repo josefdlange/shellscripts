b18312760b33acd4e69e19b65dec6ad914083d07
Josef Lange <josef.d.lange@me.com>
2012-12-28 22:38:18 -0800

    Added gob's program.

---
+++ gob.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Gob's Program: Y/N?"
+read yesno
+if [ "$yesno" == "Y" ]; then
+  for (( i = 0 ; i <= 10; i-- ))
+	do
+  	echo "PENUS PENUS PENUS PENUS PENUS PENUS"
+	done
+else
+	echo "Go away, Michael!"
+	for (( i = 0 ; i <= 10; i-- ))
+	do
+    echo "PENUS PENUS PENUS PENUS PENUS PENUS"
+	done
+fi
